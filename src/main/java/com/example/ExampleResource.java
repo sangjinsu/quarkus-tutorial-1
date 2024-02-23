619df6f319fa68db496a06f9f994760f064d8419
sangjinsu <climbplant39@kakao.com>
2024-02-23 19:50:11 +0900

    🎉 quarkus second tutorial

--- src/main/java/com/example/ExampleResource.java
+++ src/main/java/com/example/ExampleResource.java
@@ -1,11 +1,16 @@
 package com.example;
 
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import org.jboss.resteasy.reactive.RestQuery;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/hello")
 public class ExampleResource {
@@ -15,14 +20,27 @@ public class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello(@RestQuery String name) {
+    @Path("/greeting/{name}")
+    public String greeting(String name) {
+        return service.hello(name);    }
+
+    @GET
+    @Transactional
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello(@QueryParam("name") String name) {
+        Greeting greeting = new Greeting();
+        greeting.name = name;
+        greeting.persist();
         return "Hello " + name;
     }
 
     @GET
+    @Path("names")
     @Produces(MediaType.TEXT_PLAIN)
-    @Path("/greeting/{name}")
-    public String greeting(String name) {
-        return service.greeting(name);
+    public String names() {
+        List<Greeting> greetings = Greeting.listAll();
+        String names = greetings.stream().map(g-> g.name)
+                .collect(Collectors.joining (", "));
+        return "I've said hello to " + names;
     }
 }
--- src/main/java/com/example/GreetingService.java
+++ src/main/java/com/example/GreetingService.java
@@ -1,12 +1,13 @@
 package com.example;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.jboss.resteasy.reactive.RestQuery;
 
 @ApplicationScoped
 public class GreetingService {
 
-    public String greeting(String name){
-        return "hello " + name;
+    public String hello(@RestQuery String name) {
+        return "Hello " + name;
     }
 
 }
